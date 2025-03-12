2d6bd4b112eb80943919d265448154be27ca28ea
billchetch <45006134+billchetch@users.noreply.github.com>
2025-03-12 23:33:01 +0700

    asdfsd

--- Utilities/ConsoleHelper.cs
+++ Utilities/ConsoleHelper.cs
@@ -4,9 +4,23 @@ namespace Chetch.Utilities;
 
 public static class ConsoleHelper
 {
-    static public void PK(String text)
+    static public void PK(String text, params String[] args)
     {
-        System.Console.WriteLine(text);
+        System.Console.WriteLine(text, args);
         System.Console.ReadKey(true);
+    }
+
+    static public void CLR(String text, params String[] args)
+    {
+        System.Console.Clear();
+        System.Console.Write(text, args);
+    }
+
+    static public void LF(int n = 1)
+    {
+        for(int i = 0; i < n; i++)
+        {
+            Console.WriteLine("");
+        }
     }
 }
